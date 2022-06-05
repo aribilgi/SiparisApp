d7708443415a526f6f95349985200bba5c804609
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-06-05 14:05:40 +0300

    5-6-22 WebApi Eklendi

---
+++ SiparisApp.WebAPI/Controllers/SlidersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using Entities;
+
+namespace SiparisApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlidersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public SlidersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sliders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Slider>>> GetSliders()
+        {
+            return await _context.Sliders.ToListAsync();
+        }
+
+        // GET: api/Sliders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Slider>> GetSlider(int id)
+        {
+            var slider = await _context.Sliders.FindAsync(id);
+
+            if (slider == null)
+            {
+                return NotFound();
+            }
+
+            return slider;
+        }
+
+        // PUT: api/Sliders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSlider(int id, Slider slider)
+        {
+            if (id != slider.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(slider).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SliderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sliders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Slider>> PostSlider(Slider slider)
+        {
+            _context.Sliders.Add(slider);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSlider", new { id = slider.Id }, slider);
+        }
+
+        // DELETE: api/Sliders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSlider(int id)
+        {
+            var slider = await _context.Sliders.FindAsync(id);
+            if (slider == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sliders.Remove(slider);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SliderExists(int id)
+        {
+            return _context.Sliders.Any(e => e.Id == id);
+        }
+    }
+}
